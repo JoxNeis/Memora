a1c8785fe937387604f75ed47c0a8254673c9d97
Joshua Nehemia <joshuans224@gmail.com>
2026-04-22 20:29:16 +0700

    fix upload

--- App/Public/Scripts/quiz.js
+++ App/Public/Scripts/quiz.js
@@ -1,59 +1,60 @@
 import Quiz from "../../Model/Quiz.js";
 import Work from "../../Model/Work.js";
 
-//#region FIELDS
-const quiz = loadQuiz();
-const work = new Work();
-const problemSet = quiz.getProblems();
-let current_problems = null;
-//#endregion
+document.addEventListener("DOMContentLoaded", () => {
+  //#region FIELDS
+  const quiz = loadQuiz();
+  const work = new Work();
+  const problemSet = quiz.getProblems();
+  let current_problems = null;
+  //#endregion
 
-//#region ELEMENT
+  //#region ELEMENT
 
-//#endregion
+  //#endregion
 
-
-//#region FUNCTION
-//#region QUIZ
-function loadQuiz() {
-  const retrieved = sessionStorage.getItem("current_quiz");
-  if (!retrieved) {
-    window.location.href = "upload.html";
+  //#region FUNCTION
+  //#region QUIZ
+  function loadQuiz() {
+    const retrieved = sessionStorage.getItem("current_quiz");
+    if (!retrieved) {
+      window.location.href = "upload.html";
+    }
+    return Quiz.fromJSON(JSON.parse(quizData));
   }
-  return Quiz.fromJSON(JSON.parse(quizData));
-}
-//#endregion
-//#region PROBLEMSET
-function nextProblem() {
-  if (current_problems + 1 != problemSet.length) {
-    current_problems++;
-  } else {
-    submitWork();
+  //#endregion
+  //#region PROBLEMSET
+  function nextProblem() {
+    if (current_problems + 1 != problemSet.length) {
+      current_problems++;
+    } else {
+      submitWork();
+      window.location.href = "result.html";
+    }
+  }
+  function previousProblem() {
+    if (current_problems - 1 >= 0) {
+      current_problems--;
+    }
+  }
+  function showProblem(index) {}
+  //#endregion
+  //#region WORK
+  function saveAnswer(problemId, answer) {
+    work.addAnswer(new Answer(problemId, answer));
+  }
+
+  function removeAnswer(problemId) {
+    if (work.findAnswer(problemId)) {
+      work.removeAnswer(problemId);
+    }
+  }
+
+  function submitWork() {
+    sessionStorage.setItem("current_problem_set", JSON.stringify(problemSet));
+    sessionStorage.setItem("current_work", JSON.stringify(work));
     window.location.href = "result.html";
   }
-}
-function previousProblem() {
-  if (current_problems - 1 >= 0) {
-    current_problems--;
-  }
-}
-function showProblem(index) {}
-//#endregion
-//#region WORK
-function saveAnswer(problemId, answer) {
-  work.addAnswer(new Answer(problemId, answer));
-}
-
-function removeAnswer(problemId) {
-  if (work.findAnswer(problemId)) {
-    work.removeAnswer(problemId);
-  }
-}
-
-function submitWork() {
-  sessionStorage.setItem("current_problem_set", JSON.stringify(problemSet));
-  sessionStorage.setItem("current_work", JSON.stringify(work));
-  window.location.href = "result.html";
-}
-//#endregion
-//#endregion
+  //#endregion
+  //#endregion
+});
--- App/Public/Scripts/upload.js
+++ App/Public/Scripts/upload.js
@@ -1,67 +1,69 @@
 import Quiz from "../../Model/Quiz.js";
 
-//#region GET ELEMENT
-const button_download_template = document.getElementById(
-  "btn-download-template",
-);
-const upload_zone = document.getElementById("upload-zone");
-const file_input = document.getElementById("import-file");
-const start_button = document.getElementById("btn-start");
-//#endregion
+document.addEventListener("DOMContentLoaded", () => {
+  //#region GET ELEMENT
+  const button_download_template = document.getElementById(
+    "btn-download-template",
+  );
+  const upload_zone = document.getElementById("upload-zone");
+  const file_input = document.getElementById("import-file");
+  const start_button = document.getElementById("btn-start");
+  //#endregion
 
-//#region EVENT LISTENER
-button_download_template.addEventListener("click", () => {
-  downloadTemplate();
-});
-upload_zone.addEventListener("click", () => file_input.click());
-file_input.addEventListener("change", (e) => {
-  receiveFile(e);
-});
-start_button.addEventListener("click",(e)=>{
-  startQuiz();
-})
-//#endregion
+  //#region EVENT LISTENER
+  button_download_template.addEventListener("click", () => {
+    downloadTemplate();
+  });
+  upload_zone.addEventListener("click", () => file_input.click());
+  file_input.addEventListener("change", (e) => {
+    receiveFile(e);
+  });
+  start_button.addEventListener("click", (e) => {
+    startQuiz();
+  });
+  //#endregion
 
-//#region FUNCTION
-function downloadTemplate() {
-  const template_json = document.createElement("a");
-  template_json.href = "../../storage/template.json";
-  template_json.download = "memora-quiz-template.json";
-  document.body.appendChild(template_json);
-  template_json.click();
-  document.body.removeChild(template_json);
-}
-
-function receiveFile(e) {
-  const file = e.target.files[0];
-  if (file) handleFile(file);
-}
-
-function handleFile(file) {
-  if (!file.name.endsWith(".json")) {
-    showErrorModal("Invalid File Format", "File must be a json");
-    return;
+  //#region FUNCTION
+  function downloadTemplate() {
+    const template_json = document.createElement("a");
+    template_json.href = "../../storage/template.json";
+    template_json.download = "memora-quiz-template.json";
+    document.body.appendChild(template_json);
+    template_json.click();
+    document.body.removeChild(template_json);
   }
-  const reader = new FileReader();
-  reader.onload = (e) => {
-    try {
-      const json = JSON.parse(e.target.result);
-      const quiz = Quiz.fromJSON(json);
-      sessionStorage.setItem("current_quiz", JSON.stringify(json));
-    } catch (err) {
-      showErrorModal(
-        "Invalid JSON Format",
-        "Invalid JSON format: " + err.message,
-      );
-      start_button.disabled = true;
-      document.getElementById("loaded-info").style.display = "none";
-    }
-  };
-  reader.readAsText(file);
-}
 
-function startQuiz(){
-  console.log("Start Quiz!");
-  window.location.href = "quiz.html";
-}
-//#endregion
+  function receiveFile(e) {
+    const file = e.target.files[0];
+    if (file) handleFile(file);
+  }
+
+  function handleFile(file) {
+    if (!file.name.endsWith(".json")) {
+      showErrorModal("Invalid File Format", "File must be a json");
+      return;
+    }
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      try {
+        const json = JSON.parse(e.target.result);
+        const quiz = Quiz.fromJSON(json);
+        sessionStorage.setItem("current_quiz", JSON.stringify(json));
+      } catch (err) {
+        showErrorModal(
+          "Invalid JSON Format",
+          "Invalid JSON format: " + err.message,
+        );
+        start_button.disabled = true;
+        document.getElementById("loaded-info").style.display = "none";
+      }
+    };
+    reader.readAsText(file);
+  }
+
+  function startQuiz() {
+    console.log("Start Quiz!");
+    window.location.href = "quiz.html";
+  }
+  //#endregion
+});
