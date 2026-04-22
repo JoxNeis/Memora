adb6f2a3e2955b919bae3a453fc0574f2cd967c0
Joshua Nehemia <joshuans224@gmail.com>
2026-04-22 18:06:05 +0700

    add latex feature

---
+++ Controller/View.js
@@ -0,0 +1,15 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [
+      ["$", "$"],
+      ["\\(", "\\)"],
+    ],
+    displayMath: [
+      ["$$", "$$"],
+      ["\\[", "\\]"],
+    ],
+  },
+  startup: {
+    typeset: false,
+  },
+};
